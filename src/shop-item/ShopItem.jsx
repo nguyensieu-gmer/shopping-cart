281aa1b0a7827b8077d16bc3e8165603f5c82cea
Nguyen Van Sieu <217349416+nguyensieu-gmer@users.noreply.github.com>
2026-08-09 11:33:40 +0700

    Replace category of product with this title

--- src/shop-item/ShopItem.jsx
+++ src/shop-item/ShopItem.jsx
@@ -4,8 +4,8 @@ export function ShopItem({ item }) {
   const { category, price, image, title } = item;
   return (
     <li className={styles.list}>
-      <img src={image} alt={title} />
-      <h2>{category}</h2>
+      <img src={image} alt={category} />
+      <h2>{title}</h2>
       <h1>{price}$</h1>
       <button>Add to cart</button>
       <button>See options</button>
