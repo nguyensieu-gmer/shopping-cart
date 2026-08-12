d7bb345623a8bd8249a6a7b4d39709d6b0166f4b
Nguyen Van Sieu <217349416+nguyensieu-gmer@users.noreply.github.com>
2026-08-12 16:35:58 +0700

    Make cart's item can be fine-tuning and update

--- src/cart-item/CartItem.jsx
+++ src/cart-item/CartItem.jsx
@@ -1,9 +1,53 @@
+import { useOutletContext } from "react-router";
+import styles from "./CartItem.module.css";
+
 export function CartItem({ item }) {
+  const { setCartProducts } = useOutletContext();
+
+  function decreaseProductQuantity() {
+    setCartProducts((prev) =>
+      prev.map((i) =>
+        i.id === item.id && i.quantity > 1
+          ? { ...i, quantity: i.quantity - 1 }
+          : { ...i },
+      ),
+    );
+  }
+
+  function increaseProductQuantity() {
+    setCartProducts((prev) =>
+      prev.map((i) =>
+        i.id === item.id && i.quantity < 20
+          ? { ...i, quantity: i.quantity + 1 }
+          : { ...i, quantity: i.quantity },
+      ),
+    );
+  }
+
+  function hanleDeleteProduct() {
+    setCartProducts((prev) => prev.filter((i) => i.id !== item.id));
+  }
+
   return (
-    <li key={item.id}>
-      <img src={item.image} alt={item.title} />
-      <h1>{item.quantity}</h1>
-      <h2>{item.price}</h2>
+    <li className={styles.cartItem}>
+      <img src={item.image} alt="cart's product" />
+      <p>{item.title}</p>
+      <div className={styles.fineTunning}>
+        {item.quantity === 1 ? (
+          <button onClick={hanleDeleteProduct}>
+            <i className="ri-delete-bin-line"></i>
+          </button>
+        ) : (
+          <button onClick={decreaseProductQuantity}>
+            <i className="ri-subtract-line"></i>
+          </button>
+        )}
+        <p>{item.quantity}</p>
+        <button onClick={increaseProductQuantity}>
+          <i className="ri-add-line"></i>
+        </button>
+      </div>
+      <p>{Number(item.price) * Number(item.quantity)}$</p>
     </li>
   );
 }
