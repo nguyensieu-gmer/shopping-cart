1d6b5b4737933b440b17ca30f0a242b406803206
Nguyen Van Sieu <217349416+nguyensieu-gmer@users.noreply.github.com>
2026-08-12 14:37:30 +0700

    Add remixicon library and apply light style

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { RouterProvider } from "react-router";
 import router from "./Routes";
+import "remixicon/fonts/remixicon.css";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
