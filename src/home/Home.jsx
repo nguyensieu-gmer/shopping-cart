3567f60bc59bf609c2a4cfc6bc292a8cd27e8c67
Nguyen Van Sieu <217349416+nguyensieu-gmer@users.noreply.github.com>
2026-08-17 13:55:52 +0700

    Add style for home

--- src/home/Home.jsx
+++ src/home/Home.jsx
@@ -1,10 +1,17 @@
 import { Link } from "react-router";
+import styles from "./Home.module.css";
 
 export function Home() {
   return (
-    <div>
-      <h1>Hi there! What are you looking for today?</h1>
-      <Link to="/shop">Shop Now</Link>
+    <div className={styles.home_container}>
+      <div className={styles.home_content}>
+        <h1>Hi there! What are you looking for today?</h1>
+        <div className={styles.link_container}>
+          <Link className={styles.link} to="/shop">
+            Shop Now
+          </Link>
+        </div>
+      </div>
     </div>
   );
 }
