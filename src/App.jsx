b382373fe3bf53bbaf69384127c9f38cd71410f6
Nguyen Van Sieu <217349416+nguyensieu-gmer@users.noreply.github.com>
2026-08-18 08:24:38 +0700

    Add style for App

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import { Outlet } from "react-router";
 import { NavBar } from "./nav-bar/NavBar";
 import { useState } from "react";
+import style from "./App.module.css";
 
 export function App() {
   const [products, setProducts] = useState(null);
   const [cartProducts, setCartProducts] = useState([]);
   return (
-    <div>
+    <div className={style.app_container}>
       <NavBar cartProducts={cartProducts} />
       <Outlet
         context={{ products, setProducts, cartProducts, setCartProducts }}
